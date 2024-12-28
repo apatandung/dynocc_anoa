1ac2cb397ec4e3d8b29e6d85ab72821509c747a7
Alfons Patandung <apatandung@gmail.com>
2024-12-29 01:05:52 +0800

    created script

---
+++ script/anoa_2023.R
@@ -0,0 +1,7 @@
+## DYNAMIC OCCUPANCY 
+## Species : Anoa (Bubalus depressicornis)
+##==============================AUTHORS==================## 
+# WCS : Alfons Patandung, Arief Rahman
+## BTNBNW : Dini Rahmanita, Hendra D Purnama, Haydin R Faizin, Amal Hidayat, Ronny Mokoginta
+
+
