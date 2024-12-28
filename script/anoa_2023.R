affa10234c79f20a268dec3cf3c24c28a83b29c9
Alfons Patandung <apatandung@gmail.com>
2024-12-29 01:12:42 +0800

    load package

--- script/anoa_2023.R
+++ script/anoa_2023.R
@@ -5,3 +5,11 @@
 ## BTNBNW : Dini Rahmanita, Hendra D Purnama, Haydin R Faizin, Amal Hidayat, Ronny Mokoginta
 
 
+#============LOAD LIBRARY
+#Aktifkan package yang akan digunakan 
+library(dplyr) # for data organization
+library(magrittr) # for piping
+library(ggplot2) # for plotting
+library(unmarked) # for occupancy models
+library(AICcmodavg) # For GOF tests
+library(raster) # for mapping 
